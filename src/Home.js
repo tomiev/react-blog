81ef7be594375f2defb36d018d19b6f63cfe13f4
Todd Evans <tmeva2@student.monash.edu>
2022-05-04 15:59:16 +1000

    Added click events to landing page

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('Hello, world', e);
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('Hello ' + name, e.target);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain('Mario', e)}>Click me again</button>
     </div>
    );
 }
