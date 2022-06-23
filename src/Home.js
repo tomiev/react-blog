4cac1f545fd9fba96fd746b1831c40610bd9eb21
Todd Evans <evansmtodd@gmail.com>
2022-06-23 14:57:57 +1000

    Made useFetch custom hook for handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,15 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import BlogList from './BlogList';
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-      })
-  }, [])
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className="home">
-      {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title='All blogs' />}
+      { error && <div>{ error } </div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title='All blogs' /> }
     </div>
    );
 }
