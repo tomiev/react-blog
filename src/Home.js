dbccd2ecf5f5f9282398c56d2854948d3e0295c5
Todd Evans <evansmtodd@gmail.com>
2022-06-23 11:46:19 +1000

    Added conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -17,12 +13,14 @@ const Home = () => {
       })
       .then(data => {
         setBlogs(data);
+        setIsPending(false);
       })
   }, [])
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete} />}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title='All blogs' />}
     </div>
    );
 }
