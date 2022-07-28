7e7abf936cfbd87db0531ad6ed05e857081695c9
Todd Evans <evansmtodd@gmail.com>
2022-07-28 12:49:38 +1000

    Method for handling blog submission

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,17 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('Mario');
 
+  const handleSubmit = (e) => {
+    e.preventDefault(); /* Prevents default behaviour of button (which is to refresh the page) */
+    const blog = { title, body, author };
+
+    console.log(blog);
+  }
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
